f4018ca74922ea971fe936f6ad4e4d853c69d6af
Peter61045720 <peter61045720@gmail.com>
2024-05-14 19:33:45 +0200

    Improvement of graph visualization: new style for vertices and edges; zoom option; create and delete vertices and edges

--- ViewModels/GraphVisualizationViewModel.cs
+++ ViewModels/GraphVisualizationViewModel.cs
@@ -1,15 +1,13 @@
 ﻿using AdvancedCalculator.Structures;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace AdvancedCalculator.ViewModels
 {
-    // TODO: delete backup code
     public partial class GraphVisualizationViewModel : ObservableObject
     {
-        //[ObservableProperty]
-        //private BidirectionalGraph<object, IEdge<object>> _graph;
-
         [ObservableProperty]
         private PocGraph _graph;
 
@@ -19,51 +17,36 @@ namespace AdvancedCalculator.ViewModels
         [ObservableProperty]
         private string _selectedAlgorithm;
 
+        [ObservableProperty]
+        private string? _vertexId;
+
+        [ObservableProperty]
+        private string? _sourceVertexId;
+
+        [ObservableProperty]
+        private string? _targetVertexId;
+
+        [ObservableProperty]
+        private double _zoomFactor = 1.5;
+
+        [ObservableProperty]
+        private double _minZoom = 0.5;
+
+        [ObservableProperty]
+        private double _maxZoom = 2.0;
+
         public GraphVisualizationViewModel()
         {
-            //Graph = new BidirectionalGraph<object, IEdge<object>>();
-
-            //Graph.AddVertex("A");
-            //Graph.AddVertex("B");
-            //Graph.AddVertex("C");
-            //Graph.AddVertex("D");
-            //Graph.AddVertex("E");
-            //Graph.AddVertex("F");
-
-            //Graph.AddEdge(new Edge<object>("A", "B"));
-            //Graph.AddEdge(new Edge<object>("A", "D"));
-            //Graph.AddEdge(new Edge<object>("A", "F"));
-            //Graph.AddEdge(new Edge<object>("B", "A"));
-            //Graph.AddEdge(new Edge<object>("C", "F"));
-            //Graph.AddEdge(new Edge<object>("C", "F"));
-            //Graph.AddEdge(new Edge<object>("D", "E"));
-            //Graph.AddEdge(new Edge<object>("E", "B"));
-
             Graph = new PocGraph();
 
             List<PocVertex> vertices =
             [
                 new PocVertex("A"),
                 new PocVertex("B"),
-                new PocVertex("C"),
-                new PocVertex("D"),
-                new PocVertex("E"),
-                new PocVertex("F"),
             ];
 
             Graph.AddVertexRange(vertices);
-
-            Graph.AddEdgeRange(new[]
-            {
-                EdgeFactory(vertices[0], vertices[1]),
-                EdgeFactory(vertices[0], vertices[3]),
-                EdgeFactory(vertices[0], vertices[5]),
-                EdgeFactory(vertices[1], vertices[0]),
-                EdgeFactory(vertices[2], vertices[5]),
-                EdgeFactory(vertices[2], vertices[5]),
-                EdgeFactory(vertices[3], vertices[4]),
-                EdgeFactory(vertices[4], vertices[1]),
-            });
+            Graph.AddEdge(EdgeFactory(vertices[0], vertices[1]));
 
             Algorithms =
             [
@@ -81,9 +64,110 @@ namespace AdvancedCalculator.ViewModels
             SelectedAlgorithm = "BoundedFR";
         }
 
+        [RelayCommand]
+        private void AddVertex()
+        {
+            if (!string.IsNullOrWhiteSpace(VertexId))
+            {
+                var vertexToAdd = Graph.Vertices.FirstOrDefault(v => v.ID == VertexId);
+
+                if (vertexToAdd == null)
+                {
+                    Graph.AddVertex(new PocVertex(VertexId));
+                    OnPropertyChanged(nameof(Graph));
+                    VertexId = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("This vertex already exists.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Input cannot be empty. Please enter a valid value.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        [RelayCommand]
+        private void RemoveVertex()
+        {
+            var vertexToRemove = Graph.Vertices.FirstOrDefault(v => v.ID == VertexId);
+
+            if (!string.IsNullOrWhiteSpace(VertexId) && vertexToRemove != null)
+            {
+                Graph.RemoveVertex(vertexToRemove);
+                OnPropertyChanged(nameof(Graph));
+                VertexId = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("The vertex cannot be found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        [RelayCommand]
+        private void AddEdge()
+        {
+            if (!string.IsNullOrWhiteSpace(SourceVertexId) && !string.IsNullOrWhiteSpace(TargetVertexId))
+            {
+                var edgeToAdd = Graph.Edges.FirstOrDefault(e => e.Source.ID == SourceVertexId && e.Target.ID == TargetVertexId);
+
+                if (edgeToAdd == null)
+                {
+                    var sourceVertex = Graph.Vertices.FirstOrDefault(v => v.ID == SourceVertexId);
+                    var targetVertex = Graph.Vertices.FirstOrDefault(v => v.ID == TargetVertexId);
+
+                    if (sourceVertex != null & targetVertex != null)
+                    {
+                        Graph.AddEdge(EdgeFactory(sourceVertex!, targetVertex!));
+                        OnPropertyChanged(nameof(Graph));
+                        SourceVertexId = string.Empty;
+                        TargetVertexId = string.Empty;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The source or target vertex cannot be found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("This edge already exists.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Input cannot be empty. Please enter a valid value.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        [RelayCommand]
+        private void RemoveEdge()
+        {
+            if (!string.IsNullOrWhiteSpace(SourceVertexId) && !string.IsNullOrWhiteSpace(TargetVertexId))
+            {
+                var edgeToRemove = Graph.Edges.FirstOrDefault(e => e.Source.ID == SourceVertexId && e.Target.ID == TargetVertexId);
+
+                if (edgeToRemove != null)
+                {
+                    Graph.RemoveEdge(edgeToRemove);
+                    OnPropertyChanged(nameof(Graph));
+                    SourceVertexId = string.Empty;
+                    TargetVertexId = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("The edge cannot be found.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Input cannot be empty. Please enter a valid value.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
         private static PocEdge EdgeFactory(PocVertex source, PocVertex target)
         {
-            return new PocEdge($"{source.ID}to{target.ID}", source, target);
+            return new PocEdge($"{source.ID}->{target.ID}", source, target);
         }
     }
 }
